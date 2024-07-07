92c866a128a809e65081737b11f654053f582cba
Yunus Emre KESELİ <153746932+yemre-py@users.noreply.github.com>
2024-07-07 11:22:26 +0300

    first commit

---
+++ src/main/java/com/web/user/User.java
@@ -0,0 +1,19 @@
+package com.web.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
